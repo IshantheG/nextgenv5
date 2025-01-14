c3efb7694063e46b13506adb3900020d6635fedc
IshantheG <gehlautishan@gmail.com>
2025-01-14 17:16:00 -0500

    first commit

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -11,8 +11,7 @@ export default function Navbar(){
             <li><Link href = "/">Home</Link></li>
             <li><Link href = "/about">About</Link></li>
             <li><Link href = "/team">Team</Link></li>
-            <li><Link href = "/register">Register</Link></li>
-            <li><Link href = "/session">Session</Link></li>
+            <li><Link href = "/session">Iteration 1.0</Link></li>
             <li><Link href = "/getinvolved">Get Involved</Link></li>
             <li><Link href = "/contact">Contact</Link></li>
         </ul>
