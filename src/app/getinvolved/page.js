6a58c9665e69ff20a1ba208ad1fb9376facacc1e
IshantheG <143238674+IshantheG@users.noreply.github.com>
2025-01-17 19:58:19 -0500

    Update page.js

--- src/app/getinvolved/page.js
+++ src/app/getinvolved/page.js
@@ -7,23 +7,27 @@ import Navbar from "../navbar/page"
 
 export default function GetInvolved() {
     return (
-        <div className="Session-page">
+        <div className="getinvolved-page">
             <Navbar />
 
-       
+            <div style={{ backgroundColor: '#000000' }}>
 
-            <div className="header-container">
-                <div className="title-container">
-                    <h1 className="title">Get Involved</h1>
+
+
+                <div className="header-container">
+                    <div className="title-container">
+                        <h1 className="title">Get Involved</h1>
+                    </div>
                 </div>
-            </div>
 
-           
+
                 <div className="AppContainer">
                     <h1 className="Applications">Applications have closed for Iteration 1.0</h1>
                 </div>
             </div>
+        </div>
 
-           
+
     )
 }
+
