474d928e7d0323f4e83c973c632d5566884994a1
IshantheG <143238674+IshantheG@users.noreply.github.com>
2025-01-17 19:59:38 -0500

    Update page.js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,21 +8,22 @@ export default function Hero() {
 
    
     return (
+        
         <div className="hero-page">
-
-<Navbar />
-            
-
-
-
-            <div className="header-container">
-                <h1 className="main-header">NextGen Learning</h1>
-                <div>
-                    <div className="subheading-container">
-                        <h2 className="subheading">Training the <span className="next">next</span> generation.</h2>
-                    </div>
-                </div>
+        <Navbar />
+        <div style={{ backgroundColor: '#000000' }}>
+          <div className="header-container">
+            <h1 className="main-header">NextGen Learning</h1>
+            <div>
+              <div className="subheading-container">
+                <h2 className="subheading">
+                  Training the <span className="next">next</span> generation.
+                </h2>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     )
 }
+
